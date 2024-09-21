2a0a7dd8050c2df6cfd5ced4230617e8332906a2
APIVerve Team <hello@apiverve.com>
2024-09-21 01:16:17 -0500

    Bug Fixes

---
+++ MeteoriteLandingsQueryOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class MeteoriteLandingsQueryOptions {
+public string name { get; set; }
+public int mass { get; set; }
+public int year { get; set; }
+}
+
+}
